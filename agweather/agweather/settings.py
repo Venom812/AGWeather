486f6f9c1666780e432efa83c593590f833f6730
Venom812 <skorodeev@gmail.com>
2022-12-02 05:12:03 +0300

    Celery removed

--- agweather/agweather/settings.py
+++ agweather/agweather/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'datascraper',
-    'django_celery_beat',
+    # 'django_celery_beat',
     'website'
 ]
 
@@ -130,10 +130,10 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'  
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'  
-CELERY_ACCEPT_CONTENT = ['application/json']  
-CELERY_TASK_SERIALIZER = 'json'  
-CELERY_RESULT_SERIALIZER = 'json'  
-CELERY_TIMEZONE = "Europe/Moscow"
+# CELERY_BROKER_URL = 'redis://localhost:6379'  
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'  
+# CELERY_ACCEPT_CONTENT = ['application/json']  
+# CELERY_TASK_SERIALIZER = 'json'  
+# CELERY_RESULT_SERIALIZER = 'json'  
+# CELERY_TIMEZONE = "Europe/Moscow"
 
